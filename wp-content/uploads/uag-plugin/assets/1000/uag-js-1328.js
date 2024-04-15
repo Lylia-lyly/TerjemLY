6ab99cd0a9147d1e7f23f52787e484cb14a226e5
Lylia-lyly <166923436+Lylia-lyly@users.noreply.github.com>
2024-04-15 02:23:15 +0100

    Add files via upload

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1328.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"6f3ab88c","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"terjemly.lyly@gmail.com","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Soumission du formulaire","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Veuillez remplir le captcha ci-dessus.","confirmationUrl":""}, '.uagb-block-6f3ab88c', 1328 );
+});
+ });
